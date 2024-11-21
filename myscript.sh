5bbd6d18a430bbc57c138479e9eceec4fae8b519
Bitewulign Mekonnen <bitkassa2006@gmail.com>
2024-11-21 18:47:11 -0500

    new file added and modified *.sh

--- myscript.sh
+++ myscript.sh
@@ -1,26 +1,2 @@
 #!/bin/bash
-mkdir clearn_repo
-touch myscript.sh
-touch README.md
-code README.md
-cd clean_repo
-git init
-git status
-git add -A
-git status
-git commit -m "init commit"
-git checkout -b main
-git checkout -b main
-git status
-git status
-git status
-git log
-git remote add origin https://github.com/bitewulign/bit_demo-cuddly-oct-adventure.git
-git push -u origin main
-git push
-
-# To dd updates
-git add -A
-git status
-git commit -m "Updated *.sh"
-git push
+echo "Hello, World!"
